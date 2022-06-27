f6cb51d4d18a10b7538e654cc20fd130468269ba
Sampriti Das <99536474+sampritidas@users.noreply.github.com>
2022-06-28 03:05:20 +0530

    Implemented guestBookHandler and guestbook page

--- guestBookHandler.js
+++ guestBookHandler.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 
+const modifyString = (string) => {
+  return string.split('+').join(' ')
+};
+
 const getNewComment = (queries) => {
-  const date = new Date();
+  const date = (new Date() + '').slice(0, 15);
+  const time = (new Date() + '').slice(16, 21);
   const { name, comment } = queries;
-  console.log(date, name, comment);
-  return `${date} : ${name} \n\t\t ${comment}`;
+
+  const modifyComment = modifyString(comment);
+  const modifyName = modifyString(name);
+  return `${date} [${time}]: <strong>${modifyName}</strong> ---> \n\t\t ${modifyComment}\n`;
 };
 
 const readFile = (filename) => {
@@ -15,19 +22,22 @@ const appendFile = (filename, content) => {
   fs.appendFileSync(filename, content, 'utf8');
 };
 
-const updateCommentList = (newComment) => {
-  appendFile('comments.txt', newComment);
-  const allComments = readFile('./comments.txt');
+const attachTag = (comment) => {
+  return `<div class="comment">${comment}</div>`
+};
 
-  const guestBookContent = readFile('./guestBook.html');
-  return guestBookContent.replace('_COMMENTS_', allComments);
+const updateCommentList = (newComment) => {
+  const tagQuote = '<div class="last-greet">_THANKYOU_VISIT_AGAIN_</div>';
+  const commentTag = attachTag(newComment);
+  const guestBookContent = readFile('./htmls/guestBook.html');
+  return guestBookContent.replace(tagQuote, commentTag + tagQuote);
 };
 
 
 const guestBook = (response, protocol, queries) => {
   const newComment = getNewComment(queries);
   const content = updateCommentList(newComment);
-  console.log(content);
+  fs.writeFileSync('./htmls/guestBook.html', content, 'utf8');
   response.statuscode = 301;
   response.setHeader('content-type', 'text/html');
   response.send(protocol, content);
