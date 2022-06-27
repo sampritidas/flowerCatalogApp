48b254ba1d703417adb575d1841e9ce0dadc5b23
Sampriti Das <99536474+sampritidas@users.noreply.github.com>
2022-06-27 17:32:06 +0530

    Implemented firstpage secondpage and guestbook

--- catalogServer.js
+++ catalogServer.js
@@ -3,37 +3,50 @@ const { createServer } = require('net');
 const { parseChunk } = require('./parseRequest');
 const { Response } = require('./response');
 
-const parseUri = (rawUri) => {
-  const queryParams = {};
-  const [uri, params] = rawUri.split('?');
+const extentions = {
+  '.html': 'text/html',
+  '.jpeg': 'image/jpeg',
+  '.jpg': 'image/jpeg',
+  '.png': 'image/png',
+  '.pdf': 'application/pdf'
+}
 
-  if (params) {
-    const queries = params.split('&');
-    queries.forEach((param) => {
-      const [key, value] = param.split('=');
-      queryParams[key] = value;
-    })
-  }
-  return [uri, queryParams];
+const getContentType = (filename) => {
+  const indexOfExtention = filename.lastIndexOf('.');
+  const extention = filename.slice(indexOfExtention);
+  return extentions[extention] ? extentions[extention] : 'text/plain';
 };
 
-const catalog = (response, protocol, queries) => {
-  const content = fs.readFileSync('catalog.html', 'utf8');
-  console.log(content);
-  response.setHeader('content-type', 'text/html');
-  response.statuscode = 200;
-  response.send(protocol, content);
+const path = (staticRoot, uri) => {
+  return (staticRoot === undefined ? '.' : staticRoot) + uri;
+};
+
+const serveFileContent = ({ uri, protocol }, response, staticRoot) => {
+  if (uri === '/') {
+    uri = '/index.html';
+  }
+
+  const filename = path(staticRoot, uri);
+  console.log('path', filename);
+
+  if (!fs.existsSync(filename)) {
+    response.statuscode = 404;
+    response.send(protocol, 'file not found');
+    return false;
+  }
+
+  const contentType = getContentType(filename);
+  console.log('type', contentType);
+
+  const content = fs.readFileSync(filename);
+
+  response.setHeader('content-type', contentType);
+  response.send(protocol.trim(), content);
   return true;
 };
 
-const flowerCatalog = ({ protocol, uri }, response, staticRoot) => {
-  console.log(uri, response);
-  const [parsedUri, queries] = parseUri(uri);
-  console.log(parsedUri, queries);
-  if (parsedUri === '/catalog') {
-    return catalog(response, protocol.trim());
-  }
-};
+module.exports = { serveFileContent };
+
 
 const runServer = (PORT, staticRoot, handler) => {
   const server = createServer((socket) => {
@@ -51,7 +64,7 @@ const runServer = (PORT, staticRoot, handler) => {
 
 const main = (staticRoot) => {
   const PORT = 44444;
-  runServer(PORT, staticRoot, flowerCatalog);
+  runServer(PORT, staticRoot, serveFileContent);
 };
 
 main(process.argv[2]);
