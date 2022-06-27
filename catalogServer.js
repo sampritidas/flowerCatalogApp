c292d95cc9cbf93087767fcb72e727bd65ce7c8a
Sampriti Das <99536474+sampritidas@users.noreply.github.com>
2022-06-27 15:50:49 +0530

    Linked server with static html file

--- catalogServer.js
+++ catalogServer.js
@@ -1,4 +1,39 @@
+const fs = require('fs');
 const { createServer } = require('net');
+const { parseChunk } = require('./parseRequest');
+const { Response } = require('./response');
+
+const parseUri = (rawUri) => {
+  const queryParams = {};
+  const [uri, params] = rawUri.split('?');
+
+  if (params) {
+    const queries = params.split('&');
+    queries.forEach((param) => {
+      const [key, value] = param.split('=');
+      queryParams[key] = value;
+    })
+  }
+  return [uri, queryParams];
+};
+
+const catalog = (response, protocol, queries) => {
+  const content = fs.readFileSync('catalog.html', 'utf8');
+  console.log(content);
+  response.setHeader('content-type', 'text/html');
+  response.statuscode = 200;
+  response.send(protocol, content);
+  return true;
+};
+
+const flowerCatalog = ({ protocol, uri }, response, staticRoot) => {
+  console.log(uri, response);
+  const [parsedUri, queries] = parseUri(uri);
+  console.log(parsedUri, queries);
+  if (parsedUri === '/catalog') {
+    return catalog(response, protocol.trim());
+  }
+};
 
 const runServer = (PORT, staticRoot, handler) => {
   const server = createServer((socket) => {
@@ -16,7 +51,7 @@ const runServer = (PORT, staticRoot, handler) => {
 
 const main = (staticRoot) => {
   const PORT = 44444;
-  runServer(PORT);
+  runServer(PORT, staticRoot, flowerCatalog);
 };
 
 main(process.argv[2]);
